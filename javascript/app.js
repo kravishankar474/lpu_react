6553b367de14d741111fc3bfe56940fc06dfe541
RAVISHANKAR KUMAR <kravishankar474@gmail.com>
2020-08-13 11:25:31 +0530

    added stack using class

---
+++ javascript/app.js
@@ -0,0 +1,17 @@
+//tim berner lee introduces javascript
+//sum microsystems -- mozilaa parents company
+//interpreter -- line by line
+//ECMA standardisation for js
+//ES6 2015
+//DATATYPES-- number, string, null, undefined, boolean, objects, symbol(es6)
+
+var age  = 10;
+var name = 'Ravi'
+console.log(age)
+console.log(name)
+
+var phrase = 'good afternoon what\'s going on'
+console.log(phrase[10])
+
+//string 
+console.log(phrase.length)
